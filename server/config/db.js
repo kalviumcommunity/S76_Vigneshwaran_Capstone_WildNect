69f80a16330cecd8660412b11693efc630a1e8e4
Vigneshwaran-star <nanbanenemy143@gmail.com>
2025-04-17 14:12:03 +0530

    Error Handling

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,16 @@
-
-const mongoose=require("mongoose")
-const connection =()=>{mongoose
-  .connect(process.env.DB_URL)
-  .then(() => {
-    console.log("MongoDb connected successfully");
-  })
-  .catch((err) => {
-    console.log("Error connecting to the MongoDB", err);
-  });}
-module.exports=connection
+const mongoose = require("mongoose");
+const connection = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDb connected successfully");
+    })
+    .catch((err) => {
+      console.log("Error connecting to the MongoDB", err);
+      setTimeout(connection,5000)
+    });
+};
+module.exports = connection;
