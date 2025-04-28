93b3eb56753b25fad4da2021d39013964d7b92f3
Vigneshwaran-star <nanbanenemy143@gmail.com>
2025-04-28 12:08:09 +0530

    add get endpoint

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 require("dotenv").config({path:'./config/.env'});
 const express = require("express");
 const connection = require("./config/db");
+const species =require("./Routes/Species");
+const cors =require('cors')
 const app = express();
+app.use(cors())
+app.use('/species',species)
 connection();
 const port =process.env.PORT || 5000
 app.listen(port,()=>{
